28428517bbdc8500b60c49c7e21d0b0275a21cda
cycl10s <sid.chan15@gmail.com>
2018-10-27 16:53:28 +0100

    spookyskeleton

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,37 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/login')
+def login():
+    return "login "
 
-def index():
-    return "hello world"
+@app.route('/db')
+def db():
+    return "good boi, logged in"
+
+@app.route('/kb')
+def kb():
+    return "wiki"
+
+@app.route('/logout')
+def logout():
+    return "bye bye"
+
+@app.route('/allChat')
+def allChat():
+    return "chatrooms"
+
+@app.route('/oneChat/<chatyppl>')
+def oneChat(name):
+    return ""
+
+@app.route('/buddy')
+def buddy():
+    return "ppl i liked"
+
+@app.route('/forum')
+def forum():
+    return "this is a forum, sorry i couldn't think of something funny"
 
 if __name__ == "__main__":
     app.run()
