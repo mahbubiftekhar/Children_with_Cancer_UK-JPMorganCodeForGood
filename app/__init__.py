3b260145d555e98f0a3a406b465356dd76f40f99
cycl10s <sid.chan15@gmail.com>
2018-10-28 04:02:49 +0000

    dashboarding

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+
 from flask import Flask, json, request, url_for, redirect, abort, render_template
 from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
-from flask_socketio import SocketIO
 import uuid
-from datahelper import *
+import datahelper
 import os
 
 app = Flask(__name__)
@@ -10,13 +11,12 @@ app.secret_key = os.environ.get('SECRET_KEY', 'secret')
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-socket = SocketIO(app)
+
 
 active_users = {}
 
 class User(UserMixin):
     def __init__(self):
-        self.name = "Steve"
         self.id = str(uuid.uuid4())
         self.chat_id = None
 
@@ -24,11 +24,6 @@ def leave_chat(user):
     chatroom = get_chatroom_by_id(user.chat_id)
     user.chat_id = None
     chatroom.users.remove(user)
-
-def send_message(user, msg):
-    data = {'name' : user.name,
-            'msg'  : msg }
-    socket.emit(f'chatroom_{user.chat_id}', data, broadcast=True)
 
 @login_manager.user_loader
 def load_user(id):
@@ -39,26 +34,22 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        if request.headers['Content-Type'] != 'application/x-www-form-urlencoded':
-            abort('Incorrect data format')
-        if 'inputEmail' not in request.form or 'inputPassword' not in request.form:
-            abort('Missing username or password')
-        username = request.form['inputEmail']
-        password = request.form['inputPassword']
-        print(username)
-        print(password)
-        if auth(username, password):
-            user = User()
-            login_user(user)
-            active_users[user.id] = user
-            return redirect(url_for("db"))
-        else:
-            return redirect(url_for("login"))
+        return render_template('dashboard.html')
+        # username = "safe"
+        # password = "env"
+        # if datahelper.auth(username, password):
+        #     login_user(User())
+        #     return redirect(url_for("db"))
+        # else:
+        #     return redirect(url_for("login"))
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
 
 @app.route('/db')
 @login_required
 def db():
-    print("Here")
     return "good boi, logged in"
 
 @app.route('/kb')
@@ -75,7 +66,7 @@ def profile():
 def logout():
     leave_chat(current_user)
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('homepage'))
 
 @app.route('/allchat')
 @login_required
@@ -83,20 +74,20 @@ def all_chat():
     chatrooms = get_free_chatrooms(get_chatrooms())
     return "chatrooms"
 
-@app.route('/chat/<int:id>', methods = ['GET', 'POST'])
+@app.route('/chat/<int:chatyppl>')
 @login_required
 def chat(id):
-    if request.method == 'GET':
-        chatroom = get_chatroom_by_id(id)
-        chatroom.users.append(current_user)
-        current_user.chat_id = id
-        return f'In chat {id}'
-    else:
-       chatroom = get_chatroom_by_id(id)
-       message = request.data
-       chatroom.messages.append(message)
-       send_message(current_user, message)
-       return f'Posted Message in chat {id}'
+    chatroom = get_chatroom_by_id(id)
+    chatroom.users.append(current_user)
+    current_user.chat_id = id
+    return f'In chat {id}'
+#
+# @app.route('/chat/<int:chatyppl>/post', methods = ['POST'])
+# @login_required
+# def post_chat(id):
+#     chatroom = get_chatroom_by_id(id)
+#     message = request.data
+#     chatroom.messages.append(message)
 
 @app.route('/buddy')
 @login_required
